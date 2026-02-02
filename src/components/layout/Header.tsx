f83304402c4d2e9718502dccfa25db0de1645944
serhatbelen <serhat@onlay.ai>
2026-02-02 16:49:59 +0300

    Update impersonation URL in Header and VendorRow components to remove '/vendor-dashboard' path
    - Modified the URL for opening a new window with the refreshToken in both Header and VendorRow components to remove the '/vendor-dashboard' segment.
    - This change simplifies the URL structure while maintaining the functionality for user impersonation.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -88,7 +88,7 @@ export const Header = () => {
 
           if (refreshToken) {
             const b2bUrl = import.meta.env.VITE_B2B_URL;
-            window.open(`${b2bUrl}/vendor-dashboard?refreshToken=${refreshToken}`,'_blank');
+            window.open(`${b2bUrl}?refreshToken=${refreshToken}`,'_blank');
           } else {
             toast.error('Could not retrieve refreshToken for impersonation');
           }
